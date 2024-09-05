c4126c38c172d44fa1103ad522d548e7c81475aa
Anthony Edmonds <anthony.edmonds@live.co.uk>
2024-09-05 12:46:07 +0100

    Completed stubbing

--- tests/Unit/TestTest.php
+++ tests/Unit/TestTest.php
@@ -1,1 +1,13 @@
 <?php
+
+namespace AnthonyEdmonds\LaravelDatabaseLog\Tests\Unit;
+
+use AnthonyEdmonds\LaravelDatabaseLog\Tests\TestCase;
+
+class TestTest extends TestCase
+{
+    public function test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
