1ab272413bc61f2d9e5ab62e244efb731525092b
Anthony Edmonds <anthony.edmonds@live.co.uk>
2024-09-05 08:29:29 +0100

    Stubbed

--- src/DatabaseLogServiceProvider.php
+++ src/DatabaseLogServiceProvider.php
@@ -10,14 +10,14 @@ class DatabaseLogServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->publishes([
-            __DIR__.'/database-log.php' => config_path('database-log.php'),
+            __DIR__ . '/database-log.php' => config_path('database-log.php'),
         ]);
 
         $this->publishesMigrations([
-            __DIR__.'/0000_00_00_000000_create_logs_table.php' => database_path('migrations/0000_00_00_000000_create_logs_table.php'),
+            __DIR__ . '/0000_00_00_000000_create_logs_table.php' => database_path('migrations/0000_00_00_000000_create_logs_table.php'),
         ]);
 
-        $this->mergeConfigFrom(__DIR__.'/database-log.php', 'database-log');
+        $this->mergeConfigFrom(__DIR__ . '/database-log.php', 'database-log');
     }
 
     public function boot(): void
