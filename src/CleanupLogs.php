725740199c473607fe424f362258f04cf89a98e6
Anthony Edmonds <anthony.edmonds@networkrail.co.uk>
2024-11-11 08:50:49 +0000

    Channel is environment

--- src/CleanupLogs.php
+++ src/CleanupLogs.php
@@ -9,26 +9,22 @@ use Illuminate\Support\Facades\DB;
 
 class CleanupLogs extends Command implements PromptsForMissingInput
 {
-    protected $signature = 'database-log:cleanup {channel} {cutoff}';
+    protected $signature = 'database-log:cleanup {cutoff}';
 
-    protected $description = 'Remove Logs from the database on a specific channel which are older than the cutoff in days';
+    protected $description = 'Remove Logs from the database which are older than the cutoff in days';
 
     public function handle(): void
     {
-        /** @var string $channel */
-        $channel = $this->argument('channel');
-
         /** @var int $cutoff */
         $cutoff = $this->argument('cutoff');
 
-        $this->info("Removing Logs from the $channel channel older than $cutoff days...");
+        $this->info("Removing Logs older than $cutoff days...");
 
         $expiryDate = Carbon::today()
             ->subDays($cutoff)
             ->toDateTimeString();
 
         $deleted = DB::table(config('database-log.table'))
-            ->where('channel', '=', $channel)
             ->where('logged_at', '<', $expiryDate)
             ->delete();
 
@@ -38,7 +34,6 @@ class CleanupLogs extends Command implements PromptsForMissingInput
     protected function promptForMissingArgumentsUsing(): array
     {
         return [
-            'channel' => 'Which logging channel would you like to use?',
             'cutoff' => 'After how many days should Logs be deleted?',
         ];
     }
